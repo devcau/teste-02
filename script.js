f587983f6027883e3346d4ff8e4eed383ec01dc0
devcau <127689886+devcau@users.noreply.github.com>
2023-06-05 15:29:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    function sim(){
+    alert("Agora sim oficialmente casados kk:)")
+    }
+
+    function desvia(t){
+        var btn = t;
+        btn.style.position = 'absolute';
+        btn.style.bottom = geraPosicao(10, 90);
+        btn.style.left = geraPosicao(0, 100);
+        console.log("opa desviei...");
+
+    }
+
+        function geraPosicao(min, max) {
+        return Math.random() * (max - min) + "%";
+}
+
